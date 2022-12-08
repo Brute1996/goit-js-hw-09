a000564c8c228214c5e191b08e8bd502bbe905df
Sergey <sergey.cherenkov96@gmail.com>
2022-12-08 21:30:27 +0200

    ver1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('button[data-start]')
+const buttonStop = document.querySelector('button[data-stop]')
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let randomizeBodyColorActivate = true;
+let colorIntervalId = null;
+
+buttonStart.addEventListener('click', () => {
+
+    if (randomizeBodyColorActivate) {
+            colorIntervalId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    }
+
+    randomizeBodyColorActivate = false;
+})
+
+buttonStop.addEventListener('click', () => {
+
+    clearInterval(colorIntervalId)
+    document.body.style.backgroundColor = '';
+    randomizeBodyColorActivate = true
+})
+
 
