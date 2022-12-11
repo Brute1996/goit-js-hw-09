e0b761168cdca1586372ca7adc0b055fb94ff040
Serhii <97292099+Brute1996@users.noreply.github.com>
2022-12-12 00:15:59 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,46 +1,37 @@
 const form = document.querySelector('.form')
 
-
-
-
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({position, delay})
-      } else {
-        reject({position, delay})
-      }
-    }, delay)
-  }
-)}
-
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  let promiseDelay = form.elements.delay.value;
-  let promiseStep = form.elements.step.value;
-  let amount = form.elements.amount.value;
-  
-  
-  createPromise(i, promiseDelay)
-  for (let i = 1; i <= amount; i += 1) {
-    if (i === 1) {
-      createPromise(i, promiseDelay)
-    }
-  
-  }
-  
-  
+  let promiseDelay = parseInt(e.currentTarget.elements.delay.value);
+  let promiseStep = parseInt(e.currentTarget.elements.step.value);
+  let amount = parseInt(e.currentTarget.elements.amount.value);
 
+  
+  
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, promiseDelay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    promiseDelay += promiseStep;
+  }
   })
 
-// createPromise(2, 555)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+  function createPromise(position, delay) {
+    
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({position, delay})
+        } else {
+          reject({position, delay})
+        }
+      }, delay)
+    }
+  )}
+
