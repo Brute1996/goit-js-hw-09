f9d4a9f9655155a0eaaa28e8eadb2d98eab41ad7
Brute1996 <97292099+Brute1996@users.noreply.github.com>
2022-12-11 19:02:40 +0000

    Deploying to gh-pages from @ Brute1996/goit-js-hw-09@a4629895380c2796de6620bbdb98d1c64738b355 🚀

---
+++ 03-promises.705f8fa8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");e.addEventListener("input",(l=>{console.log(e.elements.delay.value),console.log(e.elements.step.value),console.log(e.elements.amount.value)})),e.addEventListener("submit",(()=>{}));
+//# sourceMappingURL=03-promises.705f8fa8.js.map
---
+++ 03-promises.91241807.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form");e.addEventListener("input",(function(n){console.log(e.elements.delay.value),console.log(e.elements.step.value),console.log(e.elements.amount.value)})),e.addEventListener("submit",(function(){}))}();
+//# sourceMappingURL=03-promises.91241807.js.map
